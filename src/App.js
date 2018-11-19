0650f158665696ce1a7560439e42aa8afb00c8f3
JLangley753 <42213489+JLangley753@users.noreply.github.com>
2018-11-19 15:08:28 +0000

    Added About section and text

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Title from './components/Title/Title';
 import Projects from './components/Projects/Projects';
 import About from './components/About/About';
+import Skills from './components/Skills/Skills';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Title />
         <Projects />
         <About />
+        <Skills />
       </div>
     );
   }
